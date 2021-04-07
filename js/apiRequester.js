374863a0d00df35385a4a5c5b280e1ff2b4cafe2
Denis <mr.danis2000@gmail.com>
2021-04-08 01:55:00 +0300

    some settings

--- js/apiRequester.js
+++ js/apiRequester.js
@@ -1,63 +1,61 @@
 const axios = require('axios').default;
 
-class ApiRequester{
+class ApiRequester {
+  constructor() {
+    this.host = process.env.APIHOST;
+    this.key = process.env.APIKEY;
+    this.pattern = 'https://weatherapi-com.p.rapidapi.com/forecast.json';
+  }
 
-    constructor(){
-        this.host = process.env.APIHOST;
-        this.key = process.env.APIKEY;
-        this.pattern = 'https://weatherapi-com.p.rapidapi.com/forecast.json';
+  convertDir(dir) {
+    const dirs = {
+      N: 'North', W: 'West', E: 'East', S: 'South',
+    };
+    let result = '';
+    for (let i = 0; i < dir.length; i += 1) {
+      result += dirs[dir[i]];
+      if (i === 0 && dir.length > 1) result += '-';
     }
+    return result;
+  }
 
-    convertDir(dir){
-        const dirs = {
-            N: 'North', W: 'West', E: 'East', S: 'South',
-          };
-          let result = '';
-          for (let i = 0; i < dir.length; i += 1) {
-            result += dirs[dir[i]];
-            if (i === 0 && dir.length > 1) result += '-';
-        }
-        return result;
+  reformatResponseJson(json) {
+    const { current } = json;
+    const { location } = json;
+
+    return {
+      city: location.name,
+      coords: {
+        lat: location.lat,
+        lon: location.lon,
+      },
+      temp: `${Math.round(current.temp_c)}°C`,
+      wind: `${current.wind_mph} m/s, ${this.convertDir(current.wind_dir)}`,
+      cloud: `${current.cloud} %`,
+      press: `${current.pressure_mb} hpa`,
+      humidity: `${current.humidity} %`,
+      icon: current.condition.icon.replace(/64x64/i, '128x128'),
+    };
+  }
+
+  async getData(cityorCoords) {
+    const options = {
+      method: 'GET',
+      url: this.pattern,
+      params: { q: cityorCoords },
+      headers: {
+        'x-rapidapi-key': this.key,
+        'x-rapidapi-host': this.host,
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      return this.reformatResponseJson(response.data);
+    } catch (error) {
+      throw error;
     }
-
-    reformatResponseJson(json){
-        const current = json.current;
-        const location = json.location;
-        
-        return {
-            city: location.name,
-            coords: {
-                lat : location.lat,
-                lon: location.lon
-            },
-            temp: `${Math.round(current.temp_c)}°C`,
-            wind: `${current.wind_mph} m/s, ${this.convertDir(current.wind_dir)}`,
-            cloud: `${current.cloud} %`,
-            press: `${current.pressure_mb} hpa`,
-            humidity: `${current.humidity} %`,
-            icon: current.condition.icon.replace(/64x64/i, '128x128') 
-        };
-    }
-
-    async getData(cityorCoords){
-
-        let options = {
-            method: 'GET',
-            url: this.pattern,
-            params: {q: cityorCoords},
-            headers: {
-                'x-rapidapi-key': this.key,
-                'x-rapidapi-host': this.host
-            }   
-        };
-
-        try {
-            const response = await axios.request(options);
-            return this.reformatResponseJson(response.data);
-        } catch (error) {
-            throw error;
-        }
-    }
+  }
 }
 
 module.exports = ApiRequester;
